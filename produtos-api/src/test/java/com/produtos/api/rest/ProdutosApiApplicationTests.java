f9bd463ca44465da3c81c8e78633174b6c9fcf1e
Diego Teodoro <diegoteodoro_alves@yahoo.com.br>
2022-03-02 14:20:35 -0300

    projeto implementado

---
+++ produtos-api/src/test/java/com/produtos/api/rest/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtos.api.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
